e8135d0e7e7ad4ee8801457a52cedf346242d779
Mark Huang <hhzeos@gmail.com>
2020-05-23 14:38:33 +0800

    Add app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+logger = logging.getLogger("Admin")
+
+logging.basicConfig(
+    filename="app.log",
+    filemode="w",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+)
+logging.warning("This will get logged to a file")
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello, World"
+
+
+if __name__ == "__main__":
+    logger.debug("--- Starting Flask Application ---")
+    app.run(host="0.0.0.0", port=5000, debug=True)
